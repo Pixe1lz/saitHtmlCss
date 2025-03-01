16175cc3a8ddb0572cea67ee898b52bebb389ad6
kirill_strelnikov <kirillstrelnikov5@gmail.com>
2025-03-01 19:32:09 +0700

    Ready registraion-site

--- checkDates.js
+++ checkDates.js
@@ -1,1 +1,21 @@
-alert("Hello")
+function validateEntry () {
+	const name = document.getElementById("getName").value; // получаем имя
+	const password = document.getElementById("getPassword").value; // получаем пароль
+	const errorMessageName = document.getElementById("errorMessageName");
+	const errorMessagePassword = document.getElementById("errorMessagePassword");
+
+	const accessDate = "admin"; // правильные данные
+
+	if (name === accessDate && password != accessDate) { // Вход по правильным данным
+		errorMessagePassword.textContent = "Неверный пароль";
+		errorMessageName.textContent = "";
+	} else if (name != accessDate && password === accessDate) {
+		errorMessageName.textContent = "Неверное имя";
+		errorMessagePassword.textContent = "";
+	} else if (name != accessDate && password != accessDate) {
+		errorMessageName.textContent = "Неверное имя";
+		errorMessagePassword.textContent = "Неверный пароль";
+	} else if (name === accessDate && password == accessDate) {
+		window.location.href = "index.html";
+	}
+}
